ef32a5491f57ecf026db8d746d3a483064cfbd69
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-04-24 19:24:33 +0430

    modalSlice file deleted and it's reducer removed from store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shoppingCartReducer from "./features/shoppingCart/shoppingCartSlice";
 import productReducer from "./features/product/productSlice";
-import modalReducer from "./features/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     shoppingCart: shoppingCartReducer,
     product: productReducer,
-    modal: modalReducer,
   },
 });
