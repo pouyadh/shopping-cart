2b8a678f8e39dd6c2d2f2e9948aec83528f4b6ab
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-04-25 01:27:44 +0430

    html main tag addd to App.jsx , also routes for home and product page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./containers/Header/Header";
 import Home from "./containers/Home/Home";
 import Modals from "./containers/Modals/Modals";
+import Product from "./containers/Product/Product";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<Product />} />
+        </Routes>
+      </main>
       <Modals />
     </>
   );
