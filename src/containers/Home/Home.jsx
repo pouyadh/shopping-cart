f942a7a5eed4bfb8b2552b093c5b76759d11130d
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-04-26 02:32:00 +0430

    some changes were made due to changes in porduct.js and product folder

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,11 +5,16 @@ import "./Home.scss";
 
 export default function Home() {
   const products = useSelector((state) => state.product.items);
+  console.log(products);
   return (
     <>
       <div className="home__cart-container">
         {products.map((item) => (
-          <Cart key={item.id} {...item} />
+          <Cart
+            key={item.id}
+            {...item}
+            img256={`/products/P-${item.id}/media/i1-256.jpg`}
+          />
         ))}
       </div>
     </>
