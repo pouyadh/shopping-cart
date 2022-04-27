a59eb7a20ab8e2374567e3495d40065e1829baf7
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-04-27 23:39:39 +0430

    progressbar added to indicate on going queries

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useIsFetching } from "react-query";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Header.scss";
+
+const Progressbar = () => {
+  const isFetching = useIsFetching();
+  return <div className="app__progressbar">{isFetching && <div />}</div>;
+};
 
 const Header = () => {
   return (
     <header>
+      <Progressbar />
       <Navbar />
     </header>
   );
