86d8aee45f645ab2771d702b0321ce4c8cefdc4d
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-04-29 02:34:43 +0430

    useIsFetching doesn't update on webkit based browsers so changed with a timer that checks isFetching state regularly

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { useIsFetching } from "react-query";
+import React, { useEffect, useState } from "react";
+import { useQueryClient } from "react-query";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Header.scss";
 
 const Progressbar = () => {
-  const isFetching = useIsFetching();
-  return <div className="app__progressbar">{isFetching && <div />}</div>;
+  const queryClinet = useQueryClient();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsLoading(queryClinet.isFetching() > 0);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [queryClinet]);
+  return <div className="app__progressbar">{isLoading && <div />}</div>;
 };
 
 const Header = () => {
