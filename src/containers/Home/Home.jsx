a2a3de9d2fe9de0587698547297af5b160b3e84b
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-02 18:40:44 +0430

    slide component added at the top of home page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { useSelector } from "react-redux";
 import Cart from "../../components/Cart/Cart";
+import Slider from "../../components/Slider/Slider";
 import "./Home.scss";
+
+const fetchSlideList = () => fetch("/slides.json").then((resp) => resp.json());
 
 export default function Home() {
   const products = useSelector((state) => state.product.items);
-  console.log(products);
+  const { isLoading, data: slideList } = useQuery("slide-list", fetchSlideList);
+
   return (
     <>
+      <div className="home__cover">
+        {!isLoading && <Slider list={slideList} autoRole withDots />}
+      </div>
       <div className="home__cart-container">
         {products.map((item) => (
           <Cart
