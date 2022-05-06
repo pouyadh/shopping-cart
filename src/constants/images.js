a4f52aa274d343d3081fd317000ba96e337f0f20
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-06 20:43:21 +0430

    removed old icons from images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,25 +1,3 @@
-import basket from "../assets/basket.svg";
-import cross from "../assets/cross.svg";
-import minus from "../assets/minus.svg";
-import plus from "../assets/plus.svg";
-import trash from "../assets/trash.svg";
 import logo from "../assets/logo.png";
-import search from "../assets/search.svg";
-import star from "../assets/star.svg";
-import arrowRight from "../assets/arrow-right.svg";
-import arrowLeft from "../assets/arrow-left.svg";
-import play from "../assets/play.svg";
 
-export {
-  basket,
-  cross,
-  minus,
-  plus,
-  trash,
-  logo,
-  search,
-  star,
-  arrowLeft,
-  arrowRight,
-  play,
-};
+export { logo };
