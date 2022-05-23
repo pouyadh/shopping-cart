a2ac95aa5095a9f0069e4da451879f2b8011d6cf
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-23 20:20:14 +0430

    Product.jsx decompostion/ create ProductMoreInfo.jsx

--- src/containers/Product/Product.jsx
+++ src/containers/Product/Product.jsx
@@ -1,46 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Product.scss";
 import { useParams } from "react-router-dom";
-
 import { useQuery } from "react-query";
-
-import Bullet from "./Bullet";
-import CartStack from "./CartStack";
-import HIT from "./HIT";
-import Image from "./Image";
-import ImageStack from "./ImageStack";
-import Note from "./Note";
-
-import Slider from "./Slider";
-import Text from "./Text";
 import { useProductsById } from "../../features/product/productSlice";
 import { productApi } from "../../APIs";
-
 import ProductMedia from "./ProductMedia";
 import ProductOverview from "./ProductOverview";
-
-const OtherSwitch = ({ idx, item, product }) => {
-  switch (item.type) {
-    case "bullet":
-      return <Bullet idx={idx} product={product} {...item} />;
-    case "note":
-      return <Note idx={idx} product={product} {...item} />;
-    case "image":
-      return <Image idx={idx} product={product} {...item} />;
-    case "image-stack":
-      return <ImageStack idx={idx} product={product} {...item} />;
-    case "cart-stack":
-      return <CartStack idx={idx} product={product} {...item} />;
-    case "slider":
-      return <Slider idx={idx} product={product} {...item} />;
-    case "HIT":
-      return <HIT idx={idx} product={product} {...item} />;
-    case "text":
-      return <Text idx={idx} product={product} {...item} />;
-    default:
-      return <></>;
-  }
-};
+import ProductMoreInfo from "./ProductsMoreInfo";
 
 const Product = () => {
   const params = useParams();
@@ -63,14 +29,7 @@ const Product = () => {
         <ProductMedia product={product} />
         <ProductOverview product={product} />
       </div>
-      {details.other.map((item, idx) => (
-        <OtherSwitch
-          key={`other-${item.type}-${idx}`}
-          idx={idx}
-          item={item}
-          product={product}
-        />
-      ))}
+      <ProductMoreInfo product={product} />
     </div>
   );
 };
---
+++ src/containers/Product/ProductsMoreInfo.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Bullet from "./Bullet";
+import CartStack from "./CartStack";
+import HIT from "./HIT";
+import Image from "./Image";
+import ImageStack from "./ImageStack";
+import Note from "./Note";
+import Slider from "./Slider";
+import Text from "./Text";
+
+const ProductsMoreInfo = ({ product }) => {
+  return product.details.other.map((item, idx) => {
+    switch (item.type) {
+      case "bullet":
+        return <Bullet key={`pmf-${idx}`} product={product} {...item} />;
+      case "note":
+        return <Note key={`pmf-${idx}`} product={product} {...item} />;
+      case "image":
+        return <Image key={`pmf-${idx}`} product={product} {...item} />;
+      case "image-stack":
+        return <ImageStack key={`pmf-${idx}`} product={product} {...item} />;
+      case "cart-stack":
+        return <CartStack key={`pmf-${idx}`} product={product} {...item} />;
+      case "slider":
+        return <Slider key={`pmf-${idx}`} product={product} {...item} />;
+      case "HIT":
+        return <HIT key={`pmf-${idx}`} product={product} {...item} />;
+      case "text":
+        return <Text key={`pmf-${idx}`} product={product} {...item} />;
+      default:
+        return <React.Fragment key={`pmf-${idx}`}></React.Fragment>;
+    }
+  });
+};
+
+export default ProductsMoreInfo;
