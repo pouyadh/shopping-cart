4f9cc6e789b15d7c6351a7b876736f0767716fe7
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-23 16:59:41 +0430

    add: dispatch fetchAllProducts on App mount and add loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./containers/Header/Header";
 import Home from "./containers/Home/Home";
@@ -6,8 +6,19 @@ import Modals from "./containers/Modals/Modals";
 import Product from "./containers/Product/Product";
 import ScrollToTopOnRouteChange from "./components/ScrollToTopOnRouteChange";
 import Footer from "./containers/Footer/Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllProducts } from "./features/product/productSlice";
 
 function App() {
+  const productState = useSelector((state) => state.product.state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, [dispatch]);
+
+  if (productState === "idle" || productState === "pending") {
+    return <div>loading products. please wait ...</div>;
+  }
   return (
     <>
       <ScrollToTopOnRouteChange />
