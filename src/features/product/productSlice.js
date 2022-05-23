bb1c069f487549e3c7b9bfee29ebd723feff4cf1
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-23 16:57:09 +0430

    create fetchAllProducts Thunk add necessary changes to productSlice.js

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,24 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
-import products from "../../constants/products";
+import { productApi } from "../../api";
 
-const productsById = products.reduce(
-  (obj, item) => ({ ...obj, [item.id]: item }),
-  {}
+export const fetchAllProducts = createAsyncThunk(
+  "products/fetchAll",
+  async () => {
+    const response = await productApi.fetchAll();
+    return response;
+  }
 );
 
+const getItemsById = (items) =>
+  items.reduce((obj, item) => ({ ...obj, [item.id]: item }), {});
+
 const initialState = {
-  items: products,
-  itemsById: productsById,
+  items: [],
+  itemsById: {},
+  state: "idle",
 };
 
 const productSlice = createSlice({
   name: "product",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllProducts.pending, (state) => {
+      state.state = "pending";
+    });
+    builder.addCase(fetchAllProducts.rejected, (state) => {
+      state.state = "failed";
+    });
+    builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
+      state.state = "success";
+      state.items = action.payload;
+      state.itemsById = getItemsById(action.payload);
+    });
+  },
 });
 
-//export {  } = productSlice.actions;
+//export { } = productSlice.actions;
 export default productSlice.reducer;
 
 export const useProducts = () => useSelector((state) => state.product.items);
